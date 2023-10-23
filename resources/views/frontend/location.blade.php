e621a71df4316f26f17a8141a67ed18d1bea44de
shakil <kazimuhammadullah@gmail.com>
2023-10-23 15:17:13 +0600

    invoice

--- resources/views/frontend/location.blade.php
+++ resources/views/frontend/location.blade.php
@@ -23,41 +23,48 @@
                 
                 <div class="row">
                     <div class="col-lg-6 text-center">
-                        <div class="card shadow-sm mx-auto mb-3">
-                          <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
-                          <h4 class="text-center my-3 fw-bold mb-1">ASF Rahman</h4>
-                          <p class="px-3 pb-3 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, velit ad aut facere cupiditate excepturi. 
-                              
-                          </p>
-                        </div>
+                        <a href="https://www.english-heritage.org.uk/visit/places/cliffords-tower-york/" target="blank">
+                            <div class="card shadow-sm mx-auto mb-3">
+                                <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
+                                <h4 class="text-center my-3 fw-bold mb-1">Clifford Tower</h4>
+                                <p class="px-3 pb-3 text-center">York Castle is a fortified complex in the city of York, England. It consists of a sequence of castles, prisons, law courts and other buildings, which were built over the last nine centuries on the south side of the River Foss.</p>
+                            </div>
+                        </a>
                     </div>
                     <div class="col-lg-6 text-center">
-                        <div class="card shadow-sm mx-auto mb-3">
-                          <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
-                          <h4 class="text-center my-3 fw-bold mb-1">ASF Rahman</h4>
-                          <p class="px-3 pb-3 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, velit ad aut facere cupiditate excepturi. 
-                              
-                          </p>
-                      </div>
+                        <a href="https://yorkminster.org/visit/plan-your-visit/" target="blank">
+                            <div class="card shadow-sm mx-auto mb-3" style="min-height: 473px">
+                                <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
+                                <h4 class="text-center my-3 fw-bold mb-1">York Minster</h4>
+                                <p class="px-3 pb-3 text-center">
+                                  
+                                </p>
+                            </div>
+                        </a>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-6 text-center">
-                        <div class="card shadow-sm mx-auto mb-3">
-                          <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
-                          <h4 class="text-center my-3 fw-bold mb-1">ASF Rahman</h4>
-                          <p class="px-3 pb-3 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, velit ad aut facere cupiditate excepturi. 
-                          </p>
-                      </div>
+
+                        <a href="https://www.visityork.org/business-directory/category/shambles" target="blank">
+                            <div class="card shadow-sm mx-auto mb-3">
+                                <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
+                                <h4 class="text-center my-3 fw-bold mb-1">Shambles ST</h4>
+                                <p class="px-3 pb-3 text-center">The Shambles is a historic street in York, England, featuring preserved medieval buildings, some dating back as far as the 14th century</p>
+                            </div>
+                        </a>
+
+                        
                     </div>
                     <div class="col-lg-6 text-center">
-                        <div class="card shadow-sm mx-auto mb-3">
-                          <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
-                          <h4 class="text-center my-3 fw-bold mb-1">ASF Rahman</h4>
-                          <p class="px-3 pb-3 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, velit ad aut facere cupiditate excepturi. 
-                          </p>
-                      </div>
+                        <a href="https://www.yorkwalls.org.uk/?page_id=3690" target="blank">
+                            <div class="card shadow-sm mx-auto mb-3">
+                                <img src="{{ asset('assets/frontend/images/d1.jpg')}}">
+                                <h4 class="text-center my-3 fw-bold mb-1">City wall</h4>
+                                <p class="px-3 pb-3 text-center">York has, since Roman times, been defended by walls of one form or another. To this day, substantial portions of the walls remain</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
